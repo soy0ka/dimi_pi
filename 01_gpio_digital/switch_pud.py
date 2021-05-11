9b1c6c36e38e9b2ee03bddb04776c4284ebc90a7
Dyij <qtcutie@kakao.com>
2021-05-11 09:51:45 +0900

    버그

--- 01_gpio_digital/switch_pud.py
+++ 01_gpio_digital/switch_pud.py
@@ -5,7 +5,7 @@ LED_PIN = 4
 SWITCH_PIN = 8
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up-down=GPIO.PUD_DOWN)
+GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 try:
